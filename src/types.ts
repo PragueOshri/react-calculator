c7f1e9ecda2b77af5871cf3021ce0cd2c12bec3f
PragueOshri <prague.oshri@gmail.com>
2021-04-17 18:55:43 +0300

    deprecated cols

--- src/types.ts
+++ src/types.ts
@@ -2,10 +2,11 @@ export type Symbol = '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' |
 export type Operation = 'x' | '÷' | '+' | '-';
 export type ButtonSymbol = Symbol | Operation;
 
+export type ButtonType = 'number' | 'operator' | 'clear' | 'result';
 export type ButtonAction = (symbol: ButtonSymbol) => void;
 
-export type ButtonType = {
+export type ButtonObject = {
     symbol: ButtonSymbol;
     action: ButtonAction;
-    cols: number;
+    type: ButtonType;
 };
